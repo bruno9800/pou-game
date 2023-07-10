fd67d05d0f3d6a48a42d6dab86ad3cc4129f7c2b
bruno9800 <bruno9800m@gmail.com>
2023-07-10 18:30:58 -0300

    jogo finalizado

--- pou.c
+++ pou.c
@@ -6,13 +6,16 @@
 #include "stb_image.h"
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
-int gameState = 0; // 0 para menu, 1 para jogo em execução
+int gameState = 0; // 0 para menu, 1 para jogo em execução;
 
 int score = 0;
 
+// image texture;
 int width, height, numChannels;
 unsigned char* image;
+GLuint textureID;
 
 // Variaveis do pou;
 float pouY = -0.4f;
@@ -20,28 +23,25 @@ float pouX = 0.0f;
 
 // Variveis da gravidade;
 float gravity = 0.008f;
-float globalY = 0.0f;
 
-// variaveis do pulo do pou
+// variaveis do pulo do pou;
 float jumpVelocity = 0.12f;
 bool isJumping = false;
 
+// variaveis de colisão;
 bool collision = false;
+float altura_tela = 0.0f;
+bool checkCollision = false;
+bool verificar_descida = false;
 
 // Variaveis do piso;
 float P_DISTANCE = 0.6f;
-
-float altura_tela = 0.0f;
-float altura_pou = -0.6f; // estado;
 
 float piso_Y[] = {-0.6, 0.0, 0.6, 1.2};
 float piso_X[] = {-0.0, 0.4, -0.2, 0.2};
 
 int ind_piso_atual = 0;
 int ind_piso_prox = 1;
-
-bool checkCollision = false;
-GLuint textureID;
 
 void loadTexture() {
     image = stbi_load("background.jpg", &width, &height, &numChannels, 0);
@@ -135,26 +135,10 @@ void piso() {
     glFlush();
 }
 
-
 void gravityAction() {
     if(!collision) {
         pouY -= gravity;
     }
-}
-
-
-int ind_menor(int atual) {
-    float menor = piso_Y[atual];
-    int ind_menor;
-
-    for(int i = 0 ; i < 4; i++) {
-        if(piso_Y[0] < menor) {
-            menor = piso_Y[i];
-            ind_menor = i;
-        }
-    }
-
-    return ind_menor;
 }
 
 bool pou_no_piso_atual() {
@@ -173,22 +157,16 @@ bool pou_passou_do_proximo_piso() {
     return pouY >= piso_Y[ind_piso_prox] + 0.08;
 }
 
-bool verificar_descida = false;
-
 void atualiza_atual(int atual){
     piso_Y[atual] += 2.4f;
     piso_X[atual] = random_x_position();
-    printf("valor atualizado: %.2f\n", piso_Y[atual]);
 }
 
 void calcular_indices(int *atual, int *prox) {
     float prox_value = piso_Y[*prox] + P_DISTANCE;
     *atual = *prox;
     *prox = (*prox + 1) % 4;
-    printf("prox_value: %.2f", prox_value);
-    printf("\natual: %d\nprox: %d\n", *atual, *prox);
 }
-
 
 void collisionEvent() {
     collision = pou_no_piso_atual();
@@ -196,7 +174,7 @@ void collisionEvent() {
    if(pouY < altura_tela - 1.4) {
     printf("game over\n");
     printf("score: %d\n", score);
-    exit(1);
+    gameState = 3;
    }
 
    if(pou_passou_do_proximo_piso()) {
@@ -229,64 +207,138 @@ void pouJump() {
         }
     }
 }
-// Função de renderização
-void renderScene() {
-    if (gameState == 0) { // Estado do menu
-        glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+// Funções de renderização
 
-        glMatrixMode(GL_PROJECTION);
-        glLoadIdentity();
-        gluOrtho2D(0, width, 0, height);
+void desenha_piso(int indice) {
+    glPushMatrix();
+        glTranslatef(piso_X[indice], piso_Y[indice], 0.0f);
+        piso();
+        glPopMatrix();
+}
 
-        glMatrixMode(GL_MODELVIEW);
-        glLoadIdentity();
 
-        glColor3f(1.0f, 1.0f, 1.0f);
+void reset() {
+    piso_Y[0] = -0.6f;
+    piso_X[0] = random_x_position();
+    for(int i = 1 ; i < 4; i++) {
+        piso_Y[i] = piso_Y[i-1] + P_DISTANCE;
+        piso_X[i] = random_x_position();
+    }
+    pouY = -0.4f;
+    pouX = piso_X[0]+0.2;
+    ind_piso_atual = 0;
+    ind_piso_prox = 1;
 
-        glRasterPos2f(10.0f, 10.0f);
 
-        const char* menuText = "Pressione Enter para iniciar o jogo";
+    altura_tela = 0.0f;
+}
 
-        for (int i = 0; menuText[i] != '\0'; i++) {
-            glutBitmapCharacter(GLUT_BITMAP_9_BY_15, menuText[i]);
+void menu_item(const char *label, float textTranslateX) {
+    glColor3f(0.0f, 1.0f, 1.0f);
+        glBegin(GL_POLYGON); // Inicia o desenho de um polígono preenchido
+        glVertex2f(-0.18f, -0.02f); // base
+        glVertex2f(0.18f, -0.02f);
+        glVertex2f(0.18f, 0.08f);
+        glVertex2f(-0.18f, 0.08f);
+        glEnd(); // Finaliza o desenho do polígono
+        glFlush();    
+        glColor3f(0.0, 0.0, 0.0); // Define a cor do texto como branco
+        glTranslatef(textTranslateX, 0.0f, 0.0f);
+        glRasterPos2f(-0.1, 0.0); // Define a posição inicial do texto
+        //const char *text = "JOGAR"; // Texto a ser exibido
+        
+        int len = strlen(label);
+        for (int i = 0; i < len; i++) {
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, label[i]); // Renderiza cada caractere do texto
         }
-    } else {
+}
+
+void game_over() {
+    char scoreStr[20];
+        glPushMatrix();
+        glTranslatef(0.0f, 0.2f, 0.0f);
+        menu_item("REINICIAR", -0.05f);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(0.0f, 0.05f, 0.0f);
+        menu_item("SAIR",0.0f);
+        glPopMatrix();
+        // score
+        glColor3f(1.0f, 0.0f, 1.0f);
+        glTranslatef(0.0f, 0.5f, 0.0f);
+        glBegin(GL_POLYGON); // Inicia o desenho de um polígono preenchido
+        glVertex2f(-0.2f, -0.04f); // base
+        glVertex2f(0.2f, -0.04f);
+        glVertex2f(0.2f, 0.1f);
+        glVertex2f(-0.2f, 0.1f);
+        glEnd(); // Finaliza o desenho do polígono
+        glFlush();
+        sprintf(scoreStr, "Score: %d", score);
+        glColor3f(0.0, 0.0, 0.0);
+        glTranslatef(-0.1f, 0.0f, 0.0f);
+        glRasterPos2i(0.0, 0.0);
+        for(int i = 0; i < strlen(scoreStr); i++){
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, scoreStr[i]);
+        }
+        glColor3f(1.0f, 0.0f, 0.0f);
+        glTranslatef(0.1f, 0.2f, 0.0f);
+        glBegin(GL_POLYGON); // Inicia o desenho de um polígono preenchido
+        glVertex2f(-0.4f, -0.04f); // base
+        glVertex2f(0.4f, -0.04f);
+        glVertex2f(0.4f, 0.1f);
+        glVertex2f(-0.4f, 0.1f);
+        glEnd();
+        glFlush();
+        const char *label = "GAME OVER";
+        glColor3f(0.0, 0.0, 0.0);
+        glTranslatef(-0.18f, 0.0f, 0.0f);
+        glRasterPos2i(0.0, 0.0);
+        for(int i = 0; i < strlen(label); i++){
+            glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, label[i]);
+        }
+
+
+        reset();
+
+}
+
+
+void renderScene() {
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
-
         glMatrixMode(GL_MODELVIEW);
         glLoadIdentity();
+    if (gameState == 0) { // Estado do menu
+
+        glPushMatrix();
+        glTranslatef(0.0f, 0.2f, 0.0f);
+        menu_item("JOGAR", 0.0f);
+        glPopMatrix();
+        glPushMatrix();
+        glTranslatef(0.0f, 0.05f, 0.0f);
+        menu_item("SAIR", 0.0f);
+        glPopMatrix();
+
+    } else if(gameState == 1) {
         gluLookAt(0.0f, altura_tela, 1.0f,
                   0.0f, altura_tela, 0.0f,
                   0.0f, 1.0f, 0.0f);
 
-        glTranslatef(0.0f, globalY, 0.0f);
         glPushMatrix();
         glTranslatef(pouX, pouY, 0.0f);
         pou();
         glPopMatrix();
-        glPushMatrix();
-        glTranslatef(piso_X[0], piso_Y[0], 0.0f);
-        piso();
-        glPopMatrix();
-        glPushMatrix();
-        glTranslatef(piso_X[1], piso_Y[1], 0.0f);
-        piso();
-        glPopMatrix();
-        glPushMatrix();
-        glTranslatef(piso_X[2], piso_Y[2], 0.0f);
-        piso();
-        glPopMatrix();
-        glPushMatrix();
-        glTranslatef(piso_X[3], piso_Y[3], 0.0f);
-        piso();
-        glPopMatrix();
+        desenha_piso(0);
+        desenha_piso(1);
+        desenha_piso(2);
+        desenha_piso(3);
+    } else if (gameState == 2) {
+        exit(1);
+    } else if (gameState == 3) {
+        game_over();
     }
     glutSwapBuffers();
 }
-
-
 
 // Função para lidar com o pressionamento das teclas
 void keyboardFunc(unsigned char key, int x, int y) {
@@ -320,6 +372,24 @@ void keyboardFunc(unsigned char key, int x, int y) {
     glutPostRedisplay(); // Atualiza a tela
 }
 
+void mouseClick(int button, int state, int x, int y) {
+    if (button == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+        // Converter as coordenadas da janela para as coordenadas do OpenGL
+        float windowX = ((float)x / glutGet(GLUT_WINDOW_WIDTH)) * 2 - 1;
+        float windowY = 1 - ((float)y / glutGet(GLUT_WINDOW_HEIGHT)) * 2;
+
+        // Verificar se o clique ocorreu dentro do retângulo do item do menu "JOGAR"
+        if (windowX >= -0.18 && windowX <= 0.18 && windowY >= 0.18 && windowY <= 0.28) {
+            gameState = 1;
+        }
+
+        // Verificar se o clique ocorreu dentro do retângulo do item do menu "SAIR"
+        if (windowX >= -0.18 && windowX <= 0.18 && windowY >= 0.02 && windowY <= 0.12) {
+            gameState = 2;
+        }
+    }
+}
+
 void gameEventLoop() {
     gravityAction();
     pouJump();
@@ -336,7 +406,7 @@ int main(int argc, char** argv) {
 
     glutDisplayFunc(renderScene);
     glutKeyboardFunc(keyboardFunc);
-
+     glutMouseFunc(mouseClick);
     glutIdleFunc(gameEventLoop);
 
     //loadTexture();
